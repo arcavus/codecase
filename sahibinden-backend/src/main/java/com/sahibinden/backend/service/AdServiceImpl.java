9d71e312dcfbf8d53acc10f0ea6ca8b86864e129
arcavus <arcavus5428@hotmail.com>
2021-03-13 00:53:11 +0300

    second commit

--- sahibinden-backend/src/main/java/com/sahibinden/backend/service/AdServiceImpl.java
+++ sahibinden-backend/src/main/java/com/sahibinden/backend/service/AdServiceImpl.java
@@ -24,6 +24,7 @@ import java.net.URL;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 @Service
 public class AdServiceImpl implements AdService {
@@ -40,29 +41,34 @@ public class AdServiceImpl implements AdService {
 
     @Override
     public AdResponse createAd(AdCreateRequest adCreateRequest) throws IOException {
-        if (AdCreateRequest.fieldControl(adCreateRequest)) return null;
-        if (isAliveLink(adCreateRequest)) return null;
-        if (titleAndDexcriptionBadWordsControl(adCreateRequest)) return null;
-        AdDocument adDocument = adMongoRepository.save(new AdDocument());
+        if (AdCreateRequest.fieldControl(adCreateRequest))
+            return null;
+        if (isAliveLink(adCreateRequest))
+            return null;
+        if (titleAndDexcriptionBadWordsControl(adCreateRequest))
+            return null;
+        AdDocument adDocument = adMongoRepository.save(adDocumentCreator(adCreateRequest));
         return new AdResponse(adDocument.getId(), adCreateRequest.getTitle(), adCreateRequest.getDescription(), adCreateRequest.getLink());
     }
 
     @Override
     public AdStatistic getAdStatistic(String adId) {
-        return null;
+        return mongoTemplate.findById(adId, AdStatistic.class);
     }
-
 
     @Override
     public DeliveryResult getWinner(MatchCriteria matchCriteria) {
         AdDocument adDocument = adMongoRepository.findByCategoryListEqualsAndClientTypeEqualsAndLocationsEquals(matchCriteria.getCategory(), matchCriteria.getClientType(), matchCriteria.getLocation());
         if (adDocument == null)
-            return null;
+            return new DeliveryResult(null, null);
+        boolean isAdDocumentSuit = (adDocument.getTotalBudget() < -2 * adDocument.getBidPrice()) || (adDocument.getFrequencyCapping() < -1l);
+        if(isAdDocumentSuit)
+            return new DeliveryResult(null, null);
         AdResponse adResponse = new AdResponse(adDocument.getId(), adDocument.getTitle(), adDocument.getDescription(), adDocument.getLink());
-        Long time = new Date().getTime();
-        DeliveryResult deliveryResult = new DeliveryResult(adResponse, time.toString());
+        UUID uuid = UUID.randomUUID();
+        DeliveryResult deliveryResult = new DeliveryResult(adResponse, uuid.toString());
         DBObject objectToSave = BasicDBObjectBuilder.start()
-                .add(time.toString(), deliveryResult)
+                .add(uuid.toString(), deliveryResult)
                 .get();
         mongoTemplate.save(objectToSave, "collection");
         return deliveryResult;
@@ -70,17 +76,34 @@ public class AdServiceImpl implements AdService {
 
     @Override
     public void processImpression(String deliveryId) {
+        impressionOperation(deliveryId);
     }
 
     @Override
     public void processClick(String deliveryId) {
-
+        impressionOperation(deliveryId);
     }
 
 
     @Override
     public void deleteAll() {
         adMongoRepository.deleteAll();
+    }
+
+    private AdDocument adDocumentCreator(AdCreateRequest adCreateRequest)
+    {
+        AdDocument adDocument = new AdDocument();
+        adDocument.setFrequencyCapping(adCreateRequest.getFrequencyCapping());
+        adDocument.setBidPrice(adCreateRequest.getBidPrice());
+        adDocument.setTotalBudget(adCreateRequest.getTotalBudget());
+        adDocument.setCategoryList(adCreateRequest.getCategoryList());
+        adDocument.setClientType(adCreateRequest.getClientType());
+        adDocument.setDescription(adCreateRequest.getDescription());
+        adDocument.setLink(adCreateRequest.getLink());
+        adDocument.setTitle(adCreateRequest.getTitle());
+        adDocument.setLocations(adCreateRequest.getLocations());
+        adDocument.setId(UUID.randomUUID().toString());
+        return adDocument;
     }
 
     private boolean isAliveLink(AdCreateRequest adCreateRequest) throws IOException {
@@ -100,10 +123,42 @@ public class AdServiceImpl implements AdService {
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             lineNum++;
-            if (line.equals(adCreateRequest.getDescription()) || line.equals(adCreateRequest.getTitle())) {
+            if (adCreateRequest.getDescription().contains(line) || adCreateRequest.getTitle().contains(line)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private void impressionOperation(String deliveryId)
+    {
+        DeliveryResult deliveryResult = mongoTemplate.findById(deliveryId, DeliveryResult.class);
+        AdDocument adDocument = adMongoRepository.findById(deliveryResult.getAdResponse().getId()).get();
+        long frequencyCapping = adDocument.getFrequencyCapping() - 1l;
+        adDocument.setFrequencyCapping(frequencyCapping);
+        Long totalBudget = adDocument.getTotalBudget() - adDocument.getBidPrice();
+        adDocument.setTotalBudget(totalBudget);
+        adMongoRepository.save(adDocument);
+
+        adStatisticControl(adDocument);
+    }
+
+    private void adStatisticControl(AdDocument adDocument)
+    {
+        AdStatistic adStatistic = mongoTemplate.findById(adDocument.getId(), AdStatistic.class);
+        if (adStatistic != null)
+        {
+            adStatistic.setImpressionCount(adStatistic.getImpressionCount() + 1l);
+            adStatistic.setClickCount(adStatistic.getClickCount() + 1l);
+        }
+        else
+        {
+            adStatistic.setAdId(adDocument.getId());
+            adStatistic.setImpressionCount(1l);
+            adStatistic.setClickCount(1l);
+        }
+
+        DBObject objectToSave = BasicDBObjectBuilder.start().add(adStatistic.getAdId(), adStatistic).get();
+        mongoTemplate.save(objectToSave, "collection");
     }
 }
